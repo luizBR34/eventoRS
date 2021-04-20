c1ad93a9ad1e88c67dc7846851761c19375c6ffa
Luiz Fernando Povoa <luizfer1@yahoo.com.br>
2021-04-20 19:39:37 -0300

    Fixing imports

--- Configs/src/main/java/com/eventoRS/configs/ResourceServerConfiguration.java
+++ Configs/src/main/java/com/eventoRS/configs/ResourceServerConfiguration.java
@@ -1,13 +1,10 @@
 package com.eventoRS.configs;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
-import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
-import org.springframework.security.oauth2.provider.token.RemoteTokenServices;
 
 
 @Configuration
@@ -19,7 +16,6 @@ public class ResourceServerConfiguration extends ResourceServerConfigurerAdapter
 	@Override 
     public void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests().antMatchers("/**").fullyAuthenticated();
-	
     }
 	
 }
